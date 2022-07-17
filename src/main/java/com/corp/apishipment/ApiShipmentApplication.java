9d920b36ac4869f27933f9e5987018c576e63e27
Gustavo Lopes <gustavolopes.dev@gmail.com>
2022-07-16 21:50:12 -0300

    first commit

---
+++ src/main/java/com/corp/apishipment/ApiShipmentApplication.java
@@ -0,0 +1,13 @@
+package com.corp.apishipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiShipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiShipmentApplication.class, args);
+	}
+
+}
