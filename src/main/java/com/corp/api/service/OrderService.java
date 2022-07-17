a0b51b89ca3b2c7b3aa19d75511ef7cfa1c7e95d
Gustavo Lopes <gustavolopes.dev@gmail.com>
2022-07-17 11:25:56 -0300

    add project structure

---
+++ src/main/java/com/corp/api/service/OrderService.java
@@ -0,0 +1,4 @@
+package com.corp.api.service;
+
+public interface OrderService {
+}
--- src/test/java/com/corp/api/ApiShipmentApplicationTests.java
+++ src/test/java/com/corp/api/ApiShipmentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.corp.apishipment;
+package com.corp.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
